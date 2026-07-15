0f5027f38a66f038b667e98088d05d665757379f
Shaik Sajida <anwarjhanshaik@gmail.com>
2026-07-15 22:50:58 +0530

    Add Queue implementation with error handling
    Implemented a queue data structure with enqueue, dequeue, and peek methods. Added error handling for underflow and overflow conditions.

---
+++ Queue/queue_list.py
@@ -0,0 +1,69 @@
+class QueueUnderflowError(Exception):
+  pass
+
+class QueueOverflowError(Exception):
+  pass
+
+class Queue:
+  def __init__(self):
+    self.__items = []
+    self.__capacity = None
+
+  def __str__(self):
+    if self.is_empty():
+      return "Queue is empty"
+    result = [str(item) for item in self.__items]
+    return f"=== QUEUE ITEMS ===\n[FRONT] {' -> '.join(result)} [REAR]"
+
+  def __iter__(self):
+    return iter(self.__items)
+
+  def __len__(self):
+    return len(self.__items)
+
+  def __repr__(self):
+    return f"Queue(size={len(self)})"
+
+  def is_empty(self):
+    return len(self) == 0
+
+  def is_full(self):
+    if self.__capacity == None:
+      return False
+  
+  def enqueue(self, data):
+    self.__items.append(data)
+
+  def dequeue(self):
+    if self.is_empty():
+      raise QueueUnderflowError("Cannot dequeue; 'Queue' is empty.")
+    return self.__items.pop(0)
+
+  def get_rear(self):
+    if self.is_empty():
+      raise QueueUnderflowError("Cannot peek rear; 'Queue' is empty.")
+    return self.__items[-1]
+
+  def get_front(self):
+    if self.is_empty():
+      raise QueueUnderflowError("Cannot peek front; 'Queue' is empty.")
+    return self.__items[0]
+
+  def clear(self):
+    self.__items.clear()
+
+q1 = Queue()
+try:
+  q1.enqueue(10)
+  q1.enqueue(20)
+  q1.enqueue(30)
+  q1.get_front()
+  q1.clear()
+  print(q1)
+except QueueUnderflowError as q:
+  print("QueueUnderflowError: ", q)
+
+
+
+
+    
